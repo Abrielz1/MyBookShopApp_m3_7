30047c3a6d87da80032e1e2810afc60868ec5e72
Abriel <abrielz11@gmail.com>
2023-09-19 10:37:31 +0300

    3rd task under construction

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,24 @@
 DROP TABLE IF EXISTS books;
 
-CREATE TABLE  books(
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       author_id BIGINT NOT NULL,
-                       title VARCHAR(250) NOT NULL,
-                       price_old INTEGER DEFAULT NULL,
-                       price INTEGER DEFAULT NULL
+CREATE TABLE books
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    author_id BIGINT       NOT NULL,
+    title     VARCHAR(250) NOT NULL,
+    price_old INTEGER DEFAULT NULL,
+    price     INTEGER DEFAULT NULL,
+    CONSTRAINT fk_author_id
+        foreign key (author_id)
+            REFERENCES authors(id)
 );
 
-CREATE TABLE authors(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY ,
-    name_author VARCHAR(320) NOT NULL UNIQUE ,
-    biography TEXT,
-    book_id BIGINT NOT NULL,
+CREATE TABLE authors
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name_author VARCHAR(320) NOT NULL UNIQUE,
+    biography   TEXT,
+    book_id     BIGINT       NOT NULL,
     CONSTRAINT fk_book_id
         FOREIGN KEY (book_id)
-            REFERENCES books(id)
+            REFERENCES books (id)
 );
