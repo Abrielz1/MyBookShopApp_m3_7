383fa48cb609cb74537ad4690c4b54a19d76b0a0
Abriel <abrielz11@gmail.com>
2023-09-27 18:43:22 +0300

    user controller fixed handler

--- src/main/java/com/example/MyBookShopApp/controllers/UserController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/UserController.java
@@ -3,14 +3,10 @@ package com.example.MyBookShopApp.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/bookshop")
 public class UserController {
-
-    //TODO
 
     @GetMapping("/signin")
     public String signinPage(){
